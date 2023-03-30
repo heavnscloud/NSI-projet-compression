123c7a37029656b56f465125bc61e50c1dcce808
Alexiscomete <wallan3100@gmail.com>
2023-03-30 11:14:28 +0200

    Schéma

---
+++ projet.py
@@ -0,0 +1,26 @@
+# ======================================== #
+# Fichier principal (et unique) du projet) #
+# ======================================== #
+
+def main():
+    pass
+
+
+def code():
+    pass
+
+
+def compte():
+    pass
+
+
+def creer_arbre():
+    pass
+
+
+def creer_table():
+    pass
+
+
+def decode():
+    pass
