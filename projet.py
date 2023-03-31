e489bec2262cc6ba94df2b2534a9d50524ee1432
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2023-03-31 09:47:09 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -3,15 +3,30 @@
 # ======================================== #
 
 def main():
-    pass
+    enco_deco = input("Voulez vous encoder ou decoder votre texte?")
+    if enco_deco == "encoder":
+        encode = input("Quel texte voulez vous encoder?")
+        return code(encode)
+    elif enco_deco == "decoder":
+        decode = input("Quel texte voulez vous decoder?")
+        return decoder(decode)
 
 
-def code():
-    pass
+def code(texte):
+    dic = compte(texte)
+    arbre = creer_arbre(dic)
+    table = creer_table(dic, arbre)
+    return encoder_txt(table, texte)
 
 
-def compte():
-    pass
+def compte(texte):
+    dic = {}
+    for caractere in texte:
+        if caractere in dic:
+            dic[caractere] += 1
+        else:
+            dic[caractere] = 1
+    return dic
 
 
 class Arbre:
@@ -95,6 +110,13 @@ def creer_table_auxiliaire(arbre, cle):
         dico1 = creer_table_auxiliaire(arbre.gauche, cle + "0")
         dico2.update(creer_table_auxiliaire(arbre.droit, cle + "1"))
         return dico1
+
+
+def encoder_txt (tab,txt):
+    liste = ''
+    for c in txt:
+        liste += tab[c]
+    return liste
 
 
 def decode():
